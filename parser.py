71b2adaf99e7a9c2a841cea479eb50469a9fda41
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2018-11-27 12:07:38 +0100

    Added functionality to change tree
    This script is somewhat done, ive added func to change a specific sub in the tree.
    Unidecode is used to Normalize the string.
    
    Also added a walkDir to be able to change multiple files.
    
    Project is pending namechange.

--- parser.py
+++ parser.py
@@ -1,17 +1,26 @@
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET, os
+from unidecode import unidecode
+from os import walk
 
-xmlFile = '.\\data\\start_chrome.xml'
+
+def getFiles():
+  dataDir = '.\\data'         # set the dir to walk
+  for files in walk(dataDir): # loop through the files in dataDir
+    return files
 
 def parseXML(file):
   tree = ET.parse(file)   # parse XML data.
   root = tree.getroot()   # get root of the data
   return tree, root       # return both for future use
 
-
 def handler():
-  tree, root = parseXML(xmlFile)    # call func and put the returned values in vars
-  for child in root[1][1][5][1]:    # loop over the root element, 1 1 5 1 is the index for Access Control -> Groups
-    print(child.text, child.attrib) # display values
+  files = getFiles()
+  for file in files:
+    tree, root = parseXML(file)                       # call func and put the returned values in vars
+    for child in root.iter('group'):                  # iterate the tree root and find "group"
+      child.text = str(unidecode(child.text))         # turn the string into a normalized string, explicitly cast as str so there wont be errors.
+
+    tree.write(file, encoding="UTF-8", method="xml")  # write the changed tree to a new file
 
 if __name__ == '__main__':
   handler()
